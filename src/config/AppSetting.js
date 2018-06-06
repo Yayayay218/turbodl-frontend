99bbcf70d285c335f433ef08fe4e7cde2926cfc5
Khan <khangds218@gmail.com>
2018-06-06 16:54:06 +0700

    Update title

--- src/config/AppSetting.js
+++ src/config/AppSetting.js
@@ -1,6 +1,7 @@
 const config = {
     URL: 'https://videodl.net/api',
     SECRET_KEY: 'AIzaSyAqf4S7NHDL1pkPpJBOvoG1MA2wjXLDNwE'
+    // SECRET_KEY: 'AIzaSyDKc0azmBrmKF0RLT2WuscONeI1i_xtSL4'
 };
 
 export default config
